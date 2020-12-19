8c7d1653d611471d4b87a7fd2730371b8f317103
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-19 02:43:31 -0700

    user login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+''' Users Views. '''
+# Django
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+
+def login_view(request):
+    ''' Login View '''
+    return render(request,'users/singin.html')
+
