dea948e86197fdedce466e52c8de3c167447db03
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-19 12:49:13 -0700

    statics

--- psociety/settings.py
+++ psociety/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "+3f#84q7m2ltz$35`(a=31=7xe@o1qkm#!fr2p=+24wp8pcin"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     DATABASES = {
