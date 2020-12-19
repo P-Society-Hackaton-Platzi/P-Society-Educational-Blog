9ca46faa914226b11600bc196f8b7ab87857f245
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-19 03:34:02 -0700

    users views

--- users/views.py
+++ users/views.py
@@ -11,5 +11,46 @@ from users.models import Profile
 
 def login_view(request):
     ''' Login View '''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('q_feed')
+        else:
+            return render(request,'users/login.html',{'error':'Invalid username and password'})
+
     return render(request,'users/singin.html')
+
+
+def sing_up_view(request):
+    """ Sign up view """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirm']
+
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password do not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username already taken'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request,'users/singup.html')
+
 
