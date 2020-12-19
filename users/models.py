884d0bc48a39a1e5d1318cba6962859290d1c16e
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-19 00:40:11 -0700

    users models

--- users/models.py
+++ users/models.py
@@ -3,4 +3,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+''' cloudinary models '''
+from cloudinary.models import CloudinaryField
 
+
+class Profile(models.model):
+    ''' Profile model extends from User base model '''
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = CloudinaryField('image')
+    menthor_points = models.IntegerField()
+    calendar = models.URLField(max_length=255)
+    availability = models.BooleanField(defautl=True)
+
+
+    def __str__(self):
+        return str(self.user.username)
